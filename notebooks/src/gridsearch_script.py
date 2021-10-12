972d3eff950f909af94ca70d63e2e5ac007eb99d
severs@uio.no <severs@uio.no>
2021-10-12 11:33:32 +0200

    Updated with grid search spec for baseline diffexp

--- notebooks/src/gridsearch_script.py
+++ notebooks/src/gridsearch_script.py
@@ -1,7 +1,10 @@
-from model_selection import XGBGridSearch
 from xgboost import XGBRegressor
+import pandas as pd
+
+from model_selection import XGBGridSearch
 from data_handling import VariableSelector
 import modules
+
 
 """
 This script tunes the model
@@ -26,10 +29,10 @@ if __name__ == "__main__":
                 }
 
     param_dist = {
-                'eta' : [0.02, 0.01, 0.04, 0.1],
-                'subsample' : [0.8, 1],                
-                'max_depth' : [3, 5, 7],         
-                'min_child_weight' : [1, 3, 5, 7],    
+                'eta' : [0.02, 0.01, 0.005],
+                'subsample' : [0.7, 0.8, 0.9, 1],                
+                'max_depth' : [2, 3, 4, 5],         
+                'min_child_weight' : [1, 2, 3, 4],    
                 }
 
     VS = VariableSelector('Cancer_baseline')
@@ -42,22 +45,25 @@ if __name__ == "__main__":
     X, y, alpha = VS.extract_data(gene, diff_lim=diff_lim, nan_limit=0.2, select_n=1000, save_corr=False, verbose=1, positive_exp=False)
 
     X_train, X_test, X_val, y_train, y_test, y_val = modules.train_test_val_split(X, y, random_state=10)
+    X_train = pd.concat([X_train, X_val])
+    y_train = pd.concat([y_train, y_val])
+
 
     tuning_model = XGBRegressor(**params)
-
+    
     GS = XGBGridSearch(tuning_model, param_dist, n_jobs=40, cv=5)
 
     search1 = GS.fit(X_train, y_train)
-
-    params['n_estimators'] = GS.iterations
-    params['learning_rate'] = GS.best_params['learning_rate']
 
     print("BEST_PARAMATERS:")
     for key in GS.best_params:
         if key in param_dist.keys():
             print(f"    {key}  :  {GS.best_params[key]}")
-
+    print(f"n_estimators = {GS.iterations}")
     
+    params['n_estimators'] = GS.iterations
+    params['learning_rate'] = GS.best_params['learning_rate']
+
     
 
     param_dist_2 = {
